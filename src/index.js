e09fd325b1ef8a52245ae3a060517f1974450905
Yoel <yoon1908@elta.co.il>
2022-08-01 15:53:38 +0300

    set hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { createTheme, ThemeProvider } from "@material-ui/core";
@@ -26,11 +26,11 @@ root.render(
     <CacheProvider value={cacheRtl}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
+        <HashRouter>
           <Provider store={store}>
             <App />
           </Provider>
-        </BrowserRouter>
+        </HashRouter>
       </ThemeProvider>
     </CacheProvider>
   </React.StrictMode>
